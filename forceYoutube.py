2f77152a7095d9644ede0c4906c78fa6a7c71a5c
Kellohylly <149101968+Kellodev@users.noreply.github.com>
2024-02-06 12:55:48 +0200

    add forceyoutube :), update readme.md

---
+++ forceYoutube.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import os, random, string
+from os import startfile
+
+videourl2 = input("URL To YouTube Video?: ")
+
+def downloadYouTube(videourl, path):
+    yt = YouTube(videourl)
+    yt_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    # Download the video and get the filename
+    filename = yt_stream.download(path)
+    return filename  # Return the full path to the downloaded file
+
+characters = string.ascii_letters + string.digits
+videoid = ''.join(random.choice(characters) for _ in range(8))
+
+newpath = r'./youtubeForceDownloadedVideos/'
+
+print(f"Video ({videourl2}) saved as 'video{videoid}' in ./youtubeForceDownloadedVideos/")
+# Capture the full path of the downloaded video
+downloaded_file_path = downloadYouTube(videourl2, newpath)
+
+# Now, use the actual downloaded file path
+startfile(downloaded_file_path)
